00f964c655b544fb270d7f2621e5ae3ea670a2c9
therne <retail3210@gmail.com>
2019-02-12 19:28:23 +0900

    Database implementation (WIP)

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/airbloc/airframe/database"
+	"github.com/pkg/errors"
 	"net/http"
+	"os"
 	"runtime"
 
 	"github.com/airbloc/logger"
@@ -15,20 +18,32 @@ func main() {
 	log := logger.New("main")
 	log.Info("Using %s configuration", config.Profile)
 
-	testWebServer(config)
+	db, err := initDatabase(config.Backend)
+	if err != nil {
+		log.Error("error: failed to initialize database", err)
+		os.Exit(1)
+	}
+	startServer(db, config.Port)
 }
 
-func testWebServer(config *Config) {
+func initDatabase(backendType string) (database.Database, error) {
+	if backendType == "memory" {
+		return database.NewInMemoryDatabase()
+	}
+	return nil, errors.Errorf("unknown backend: %s", backendType)
+}
+
+func startServer(db database.Database, port int) {
 	log := logger.New("webserver")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
+		// TODO: implement me
 	})
 
 	// server listen
-	log.Info("Server listening at http://localhost:%d", config.Port)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", config.Port), mux); err != http.ErrServerClosed {
+	log.Info("Server listening at http://localhost:%d", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux); err != http.ErrServerClosed {
 		log.Error("failed to start HTTP server", err)
 	}
 }
