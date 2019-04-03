7f95d7dc64d0ee657e29bdf3dd9561178a4bdf10
therne <retail3210@gmail.com>
2019-04-03 12:19:19 +0900

    dynamodb: Add DynamoDatabase

--- database/dynamodb/dynamodb.go
+++ database/dynamodb/dynamodb.go
@@ -1,4 +1,172 @@
 // package dynamodb implements DynamoDB backend interface.
-package dynamodb
+package dynamodatabase
 
+import (
+	"context"
+	"github.com/airbloc/airframe/database"
+	awsclient "github.com/aws/aws-sdk-go/aws/client"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/guregu/dynamo"
+	"github.com/pkg/errors"
+	"time"
+)
 
+var (
+	reservedFields = []string{"ID", "Data", "Owner", "CreatedAt", "LastUpdatedAt"}
+
+	dynamoOperators = map[database.OperatorType]string{
+		database.OpEquals:             "$ = ?",
+		database.OpGreaterThan:        "$ > ?",
+		database.OpGreaterThanOrEqual: "$ >= ?",
+		database.OpLessThan:           "$ < ?",
+		database.OpLessThanOrEqual:    "$ <= ?",
+		database.OpContains:           "contains($, ?)",
+	}
+)
+
+type DynamoDatabase struct {
+	svc *dynamo.DB
+
+	tablePrefix string
+}
+
+func New(session awsclient.ConfigProvider) *DynamoDatabase {
+	return &DynamoDatabase{
+		svc: dynamo.New(session),
+
+		tablePrefix: "airbloc_",
+	}
+}
+
+func (db *DynamoDatabase) Get(ctx context.Context, typ, id string) (*database.Object, error) {
+	table := db.svc.Table(db.tablePrefix + typ)
+
+	items := make(map[string]*dynamodb.AttributeValue)
+	if err := table.Get("ID", id).OneWithContext(ctx, &items); err != nil {
+		if err == dynamo.ErrNotFound {
+			return nil, database.ErrNotExists
+		}
+		return nil, errors.Wrap(err, "failed to get item from DynamoDB")
+	}
+
+	// trick: copy the data attrs into Data object, since the result is flattened
+	items["Data"] = &dynamodb.AttributeValue{M: make(map[string]*dynamodb.AttributeValue)}
+CopyData:
+	for key, value := range items {
+		for _, reserved := range reservedFields {
+			if key == reserved {
+				continue CopyData
+			}
+		}
+		items["Data"].M[key] = value
+	}
+
+	// now we can unmarshal it xD
+	obj := database.Object{}
+	if err := dynamo.UnmarshalItem(items, &obj); err != nil {
+		return nil, err
+	}
+	return &obj, nil
+}
+
+func (db *DynamoDatabase) Exists(ctx context.Context, typ, id string) (bool, error) {
+	table := db.svc.Table(db.tablePrefix + typ)
+	count, err := table.Get("ID", id).Count()
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get item from DynamoDB")
+	}
+	return count > 0, nil
+}
+
+func (db *DynamoDatabase) Query(ctx context.Context, typ string, query *database.Query, skip, limit int) ([]*database.Object, error) {
+	table := db.svc.Table(db.tablePrefix + typ)
+
+	// TODO: Use Query instead of Scan.
+	q := table.Scan()
+	for _, op := range query.Conditions {
+		filter := dynamoOperators[op.Type]
+		q.Filter(filter, op.Field, op.Operand)
+	}
+	q.Limit(int64(skip + limit))
+
+	var items []map[string]*dynamodb.AttributeValue
+	if err := q.AllWithContext(ctx, &items); err != nil {
+		return nil, errors.Wrap(err, "failed to scan item from DynamoDB")
+	}
+	items = items[skip:]
+
+	results := make([]*database.Object, len(items))
+	for i := 0; i < len(items); i++ {
+		item := items[i]
+
+		// trick: copy the data attrs into Data object, since the result is flattened
+		item["Data"] = &dynamodb.AttributeValue{M: make(map[string]*dynamodb.AttributeValue)}
+	CopyData:
+		for key, value := range item {
+			for _, reserved := range reservedFields {
+				if key == reserved {
+					continue CopyData
+				}
+			}
+			item["Data"].M[key] = value
+		}
+
+		// now we can unmarshal it xD
+		obj := database.Object{}
+		if err := dynamo.UnmarshalItem(item, &obj); err != nil {
+			return nil, err
+		}
+		results[i] = &obj
+	}
+	return results, nil
+}
+
+func (db *DynamoDatabase) Put(ctx context.Context, typ, id string, data database.Payload, signature []byte) (*database.PutResult, error) {
+	created := false
+	obj, err := db.Get(ctx, typ, id)
+	if err == database.ErrNotExists {
+		// create new
+		obj = &database.Object{
+			ID:   id,
+			Type: typ,
+
+			CreatedAt:     time.Now(),
+			LastUpdatedAt: time.Now(),
+		}
+		if obj.Owner, err = database.GetOwnerFromSignature(obj, signature); err != nil {
+			return nil, errors.Wrap(err, "invalid signature")
+		}
+		created = true
+
+	} else if err == nil {
+		// update object
+		obj.Data = data
+		if !database.IsOwner(obj, signature) {
+			return nil, database.ErrNotAuthorized
+		}
+		obj.LastUpdatedAt = time.Now()
+
+	} else {
+		return nil, errors.Wrap(err, "error while checking existence")
+	}
+
+	item, err := dynamo.MarshalItem(obj)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to marshal data")
+	}
+
+	// flatten the obj.Data
+	for k, v := range item["Data"].M {
+		item[k] = v
+	}
+	delete(item, "Data")
+
+	table := db.svc.Table(db.tablePrefix + typ)
+	if err := table.Put(item).RunWithContext(ctx); err != nil {
+		return nil, errors.Wrap(err, "failed to write to DynamoDB")
+	}
+	return &database.PutResult{
+		FeeUsed: 0,
+		Created: created,
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,12 @@ module github.com/airbloc/airframe
 
 require (
 	github.com/airbloc/logger v1.1.3
+	github.com/aws/aws-sdk-go v1.19.7
 	github.com/btcsuite/btcd v0.0.0-20190209000034-12ce2fc7d321 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/ethereum/go-ethereum v1.8.22
 	github.com/gin-gonic/gin v1.3.0
 	github.com/golang/protobuf v1.2.1-0.20181127190454-8d0c54c12466
+	github.com/guregu/dynamo v1.2.1
 	github.com/json-iterator/go v1.1.5
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -16,10 +17,8 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/pflag v1.0.3
 	github.com/stretchr/testify v1.3.0
-	golang.org/x/crypto v0.0.0-20190211182817-74369b46fc67
-	golang.org/x/net v0.0.0-20190206173232-65e2d4e15006 // indirect
+	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
-	golang.org/x/sys v0.0.0-20190209173611-3b5209105503 // indirect
 	golang.org/x/text v0.3.1-0.20180807135948-17ff2d5776d2 // indirect
 	google.golang.org/genproto v0.0.0-20190201180003-4b09977fb922 // indirect
 	google.golang.org/grpc v1.18.0
